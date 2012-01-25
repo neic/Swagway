ba876ee3045b1865f835cf3446f451b5ed63c9c2
Mathias Dannesbo <neic@neic.dk>
2012-01-25 17:21:38 +0100

    Code tidy-up

--- Software/swagway/swagway.ino
+++ Software/swagway/swagway.ino
@@ -1,14 +1,18 @@
 #include <Wire.h>
-#include <math.h> 
+#include <math.h>
 
-int xa, ya, za, xg, yg, zg;
-double zgDeg = 0;
-double zgDegOld = 0;
-long timeOld = 0;
-double gyroAngle = 0;
-double accAngle = 0;
+unsigned long timeNow = 0;
+unsigned long timeOld = 0;
+
 byte buffa[6];
 byte buffg[6];
+
+int xa, ya, za, xg, yg, zg;
+
+double gyroAngle = 0;
+double accAngle = 0;
+
+double zgDeg = 0;
 
 // acc I2C
 const int accaddr = 0x53;
@@ -23,35 +27,35 @@ const float gyroSens = 14375; //LSB per deg/sec
 void setup() 
 {
   Serial.begin(9600);
-  Wire.begin(); //join i2c bus (address optional for master)
+  Wire.begin();
 
-  //turning on the ADXL345
+  //Turning on the acc
   writeTo(accaddr, 0x2D, B00000000); //Resets POWER_CTL
   writeTo(accaddr, 0x2D, B00010000); //Puts the sensor to standby mode
   writeTo(accaddr, 0x2D, B00001000); //Puts the sensor to measure mode
 
-  //gyro
+  //Turning on the gyro
   writeTo(gyroregaddr, 0x16, B00010011); //Set DLPF register to FS_SEL = 3 and DLPF_cfg = 3 (LPF=42Hz, internal sample rate = 1kHz)
   writeTo(gyroregaddr, 0x15, 9); //Sets sample rate to (internal sample rate)/(9 + 1) (1kHz/(9+1)=100Hz <=> 10ms)
 }
 
 void loop() 
-{//Gyro scale 14375 LSB/deg/sec
-  unsigned long timeNow = millis();
+{
+  timeNow = millis();
 
   //Accel calculations
-  readFrom(accaddr, accdataregaddr, 6, buffa); //read the acceleration data from the ADXL345
+  readFrom(accaddr, accdataregaddr, 6, buffa); // read the data from the acc
 
-  xa=(((int)buffa[1])<<8) | buffa[0];
+  xa=(((int)buffa[1])<<8) | buffa[0]; // cleanup the data and put it in variables
   ya=(((int)buffa[3])<<8) | buffa[2];
   za=(((int)buffa[5])<<8) | buffa[4];
 
-  float xaf=xa;
+  float xaf=xa; // convert to float to do calculations
   float yaf=ya;
 
-  accAngle = atan(xaf/yaf)*180/3.14;
+  accAngle = atan(xaf/yaf)*180/3.14; // calcutalte the X-Y-angle
 
-  if (yaf > 0)
+  if (yaf > 0) // ignore angles ±90 deg
   {    
     if (xaf < 0)
     {
@@ -63,16 +67,15 @@ void loop()
     }
   }
 
-  //Gyro calculations  
-  readFrom(gyroregaddr, gyrodataregaddr, 6, buffg); //read the gyro data from ITG3200
+  //Gyro calculations
+  readFrom(gyroregaddr, gyrodataregaddr, 6, buffg); // read the data from gyro
 
-  xg=(((int)buffg[0])<<8) | buffg[1];
+  xg=(((int)buffg[0])<<8) | buffg[1]; // cleanup the data and put it in variables
   yg=(((int)buffg[2])<<8) | buffg[3];
   zg=(((int)buffg[4])<<8) | buffg[5];
 
-  zgDeg = zg / gyroSens;
-  gyroAngle = gyroAngle+(zgDeg *(timeNow-timeOld)); //bugged, showing twice the angle
-  zgDegOld = zgDeg;
+  zgDeg = zg / gyroSens; // calculate the angle change in this sample
+  gyroAngle = gyroAngle+(zgDeg *(timeNow-timeOld)/2); // Intergrate to get the absolute angle. (Why shoud this be divided by two?)
 
   //SerialDebugRaw();
   SerialDebugAngle();
@@ -95,15 +98,17 @@ void readFrom(int device, byte address, int num, byte buff[])
   Wire.write(address); //sends address to read from
   Wire.endTransmission(); //ends transmission
 
-    Wire.beginTransmission(device); //start transmission to device (initiate again)
+  Wire.beginTransmission(device); //start transmission to device (initiate again)
   Wire.requestFrom(device, num); //request 6 num bytes from device
 
   int i=0;
+
   while(Wire.available()) //device may send less than requested (abnormal)
   {
     buff[i]=Wire.read(); //receive a num byte
     i++;
   }
+  
   Wire.endTransmission(); //end transmission
 }
 void SerialDebugRaw()
@@ -130,16 +135,10 @@ void SerialDebugRaw()
 
 void SerialDebugAngle()
 {
-  /*Serial.print("gyroAngle= ");
-   Serial.println(gyroAngle);
-  /* Serial.print("zgDeg= ");
-   Serial.println(zgDeg);*/
-  Serial.print("accAngle= ");
+  Serial.print("gyroAngle= ");
+  Serial.print(gyroAngle);
+  Serial.print(" zgDeg= ");
+  Serial.print(zgDeg);
+  Serial.print(" accAngle= ");
   Serial.println(accAngle);
-  Serial.print("xa: ");
-  Serial.print(xa);
-  Serial.print(" ");
-  Serial.print("ya: ");
-  Serial.print(ya);
 }
-
