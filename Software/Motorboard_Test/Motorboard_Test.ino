6c285f89639fdf83aa1f565978ff26617506d91f
Carl Emil Groen Christensen <roberto_carlee@hotmail.com>
2012-03-17 05:05:44 +0100

    Better structured test

--- Software/Motorboard_Test/Motorboard_Test.ino
+++ Software/Motorboard_Test/Motorboard_Test.ino
@@ -2,22 +2,28 @@
 int motorpina = 2;
 int motorpinb = 3;
 
+int tm=1000;
+
 void setup() {
 pinMode(motorpina, OUTPUT);
 pinMode(motorpinb, OUTPUT);
-
+Serial.begin(9600);
 }
 
 void loop() {
 digitalWrite(motorpina, LOW);
 digitalWrite(motorpinb, LOW);
-delay(500);
+Serial.println("SHORT: - 1: Both LOW");
+delay(tm);
 digitalWrite(motorpina, HIGH);
-delay(500);
+Serial.println("RIGHT - 2: a HIGH, b LOW");
+delay(tm);
 digitalWrite(motorpina, LOW);
 digitalWrite(motorpinb, HIGH);
-delay(500);
+Serial.println("OFF - 3: a LOW, b HIGH");
+delay(tm);
 digitalWrite(motorpina, HIGH);
 digitalWrite(motorpinb, HIGH);
-delay(500);
+Serial.println("LEFT - 4: Both HIGH");
+delay(tm);
 }
