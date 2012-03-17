836cd891a8a176d81f65eea314dec08fca1c714f
Carl Emil Groen Christensen <roberto_carlee@hotmail.com>
2012-03-17 13:36:07 +0100

    Test with PWM

--- Software/Motorboard_Test/Motorboard_Test.ino
+++ Software/Motorboard_Test/Motorboard_Test.ino
@@ -1,29 +1,57 @@
 //
 int motorpina = 2;
-int motorpinb = 3;
+int motorpinb = 6; //PWM
+int motorpinc = 5; //PWM
 
 int tm=1000;
+boolean r = true;
+int value = 0;
 
 void setup() {
 pinMode(motorpina, OUTPUT);
 pinMode(motorpinb, OUTPUT);
+pinMode(motorpinc, OUTPUT);
+pinMode(A0, INPUT);
+pinMode(7, INPUT);
 Serial.begin(9600);
 }
 
 void loop() {
-digitalWrite(motorpina, LOW);
+  value = map(analogRead(A0),0,1024,-255,255);
+  
+  r = digitalRead(7);
+  if(value<
+  0)
+  {
+// R
+digitalWrite(motorpina, HIGH);
 digitalWrite(motorpinb, LOW);
-Serial.println("SHORT: - 1: Both LOW");
-delay(tm);
-digitalWrite(motorpina, HIGH);
-Serial.println("RIGHT - 2: a HIGH, b LOW");
-delay(tm);
+
+Serial.println(value);
+analogWrite(motorpinc, value);
+
+  }
+  else
+  {
+    // L
+    digitalWrite(motorpinc, HIGH);
 digitalWrite(motorpina, LOW);
-digitalWrite(motorpinb, HIGH);
-Serial.println("OFF - 3: a LOW, b HIGH");
-delay(tm);
-digitalWrite(motorpina, HIGH);
-digitalWrite(motorpinb, HIGH);
-Serial.println("LEFT - 4: Both HIGH");
-delay(tm);
+int value2 = map(value,-255,0
+,0,255);
+Serial.println(value2);
+analogWrite(motorpinb, value2);
+  }
+//Serial.println("SHORT: - 1: Both LOW");
+//delay(tm);
+//digitalWrite(motorpina, HIGH);
+//Serial.println("RIGHT - 2: a HIGH, b LOW");
+//delay(tm);
+//digitalWrite(motorpina, LOW);
+//digitalWrite(motorpinb, HIGH);
+//Serial.println("OFF - 3: a LOW, b HIGH");
+//delay(tm);
+//digitalWrite(motorpina, HIGH);
+//digitalWrite(motorpinb, HIGH);
+//Serial.println("LEFT - 4: Both HIGH");
+//delay(tm);
 }
